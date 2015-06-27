cb079af6b21e1859ff25d2d528bda0006ea5a59b
1stPayGateway.Net <support@1stpaygateway.net>
2015-06-27 11:17:59 -0400

    class name and response checking
    Class name set to RestGateway to avoid confusion; minor changes to
    gateway response checking for when the gateway returns a response that
    was not expected.

--- gateway.py
+++ gateway.py
@@ -11,8 +11,8 @@ else:
 
 global version
 
-class Blaze:
-    """ Author: Patrick Petrino | Last Revision: 6/15/2015 | Version: 1.0.0 | This class is required for all Python code making a call to 1stPayBlaze. Please refer to the gateway documentation web page for specifics on what parameters to use for each call."""
+class RestGateway:
+    """ Author: Patrick Petrino | Last Revision: 6/27/2015 | Version: 1.0.0 | This class is required for all Python code making a call to 1stPayBlaze. Please refer to the gateway documentation web page for specifics on what parameters to use for each call."""
     def __init__(self, transactionData):
         self.version = "1.0.0"
         self.apiUrl = "https://secure.1stpaygateway.net/secure/1stPayClientProxy/Gateway/Transaction/"
@@ -38,25 +38,25 @@ class Blaze:
         response = json.loads(results.text)
         self.responsecode = str(results.status_code)
         self.raw_response = dict(response)
-        if response['isError'] == True:
+        if 'isError' in response and response['isError'] == True:
             self.status = 'Error'
             self.result['isError'] = response['isError']
             self.result['errors'] = response['errorMessages']
             self.result['isValid'] = False
             self.result['validations'] = response['validationFailures']
             return self.result
-        elif response['validationHasFailed'] == True:
+        elif 'validationHasFailed' in response and response['validationHasFailed'] == True:
             self.status = 'Validation'
             self.result['isError'] = response['isError']
             self.result['errors'] = response['errorMessages']
             self.result['isValid'] = False
             self.result['validations'] = response['validationFailures']
             return self.result
-        elif response['isSuccess'] == True:
+        elif 'isSuccess' in response and response['isSuccess'] == True:
             self.status = 'Success'
             if (isinstance(response['data'], dict)) and ('isPartial' in response['data'].keys()) and(response['data']['isPartial'] == True):
                  #To pull partial response must 1) check if the response data is in fact a dictionary, 2) if 'isPartial' is present, and 3) if it is True.
-                 #Due to some of the more irregular responses from 1stpayblaze methods (i.e. getAchCategories) there is no other way to do this.
+                 #Due to some of the more irregular responses from gateway methods (i.e. getAchCategories) there is no other way to do this.
                  self.result['partialOrder'] = dict()
                  self.result['partialOrder']['partialId'] = response['data']['partialId']
                  self.result['partialOrder']['amountRemaining'] = (response['data']['originalFullAmount'] - response['data']['partialAmountApproved'])
@@ -66,7 +66,6 @@ class Blaze:
                  self.result['authCode'] = response['data']['authCode']
                  return self.result
             else:
-                 #self.result['data'] = response['data']
                  self.result = response['data']
                  return self.result
         else:
@@ -75,173 +74,173 @@ class Blaze:
     
     def createAuth(self):
         self.apiRequest = urljoin(self.apiUrl,"Auth")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createAuthUsing1stPayVault(self):
         self.apiRequest = urljoin(self.apiUrl,"AuthUsingVault")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createSale(self):
         self.apiRequest = urljoin(self.apiUrl,"Sale")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
         
     def createSaleUsing1stPayVault(self):
         self.apiRequest = urljoin(self.apiUrl,"SaleUsingVault")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createCredit(self):
         self.apiRequest = urljoin(self.apiUrl,"Credit")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createCreditRetailOnly(self):
         self.apiRequest = urljoin(self.apiUrl,"CreditRetailOnly")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createCreditRetailOnlyUsing1stPayVault(self):
         self.apiRequest = urljoin(self.apiUrl,"CreditRetailOnlyUsingVault")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def performVoid(self):
         self.apiRequest = urljoin(self.apiUrl,"Void")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
 
     # The partial void function is not currently used. Specify transactionAmount when running void instead.  
     def performPartialVoid(self):
         self.apiRequest = urljoin(self.apiUrl,"VoidPartial")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createReAuth(self):
         self.apiRequest = urljoin(self.apiUrl,"ReAuth")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createReSale(self):
         self.apiRequest = urljoin(self.apiUrl,"ReSale")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createReDebit(self):
         self.apiRequest = urljoin(self.apiUrl,"ReDebit")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def query(self):
         self.apiRequest = urljoin(self.apiUrl,"Query")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def closeBatch(self):
         self.apiRequest = urljoin(self.apiUrl,"CloseBatch")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
 
     def performSettle(self):
         self.apiRequest = urljoin(self.apiUrl,"Settle")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def applyTipAdjust(self):
         self.apiRequest = urljoin(self.apiUrl,"TipAdjust")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def performAchVoid(self):
         self.apiRequest = urljoin(self.apiUrl,"AchVoid")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createAchCredit(self):
         self.apiRequest = urljoin(self.apiUrl,"AchCredit")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createAchDebit(self):
         self.apiRequest = urljoin(self.apiUrl,"AchDebit")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createAchCreditUsing1stPayVault(self):
         self.apiRequest = urljoin(self.apiUrl,"AchCreditUsingVault")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createAchDebitUsing1stPayVault(self):
         self.apiRequest = urljoin(self.apiUrl,"AchDebitUsingVault")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def getAchCategories(self):
         self.apiRequest = urljoin(self.apiUrl,"AchGetCategories")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
 		
     def createAchCategories(self):
          self.apiRequest = urljoin(self.apiUrl,"AchCreateCategory")
-         return(Blaze.performRequest(self))
+         return(RestGateway.performRequest(self))
 
     def deleteAchCategories(self):
          self.apiRequest = urljoin(self.apiUrl,"AchDeleteCategory")
-         return(Blaze.performRequest(self))
+         return(RestGateway.performRequest(self))
 
     def setupAchStore(self):
          self.apiRequest = urljoin(self.apiUrl,"AchSetupStore")
-         return(Blaze.performRequest(self))
+         return(RestGateway.performRequest(self))
     
     def createVaultContainer(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultCreateContainer")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createVaultAchRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultCreateAchRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def createVaultCreditCardRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultCreateCCRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
 
     def createVaultShippingRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultCreateShippingRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def deleteVaultContainerAndAllAsscData(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultDeleteContainerAndAllAsscData")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def deleteVaultAchRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultDeleteAchRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def deleteVaultCreditCardRecord(self):       
         self.apiRequest = urljoin(self.apiUrl,"VaultDeleteCCRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def deleteVaultShippingRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultDeleteShippingRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def updateVaultContainer(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultUpdateContainer")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def updateVaultAchRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultUpdateAchRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def updateVaultCreditCardRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultUpdateCCRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def updateVaultShippingRecord(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultUpdateShippingRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def queryVaults(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultQueryVault")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def queryVaultForCreditCardRecords(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultQueryCCRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def queryVaultForAchRecords(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultQueryAchRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def queryVaultForShippingRecords(self):
         self.apiRequest = urljoin(self.apiUrl,"VaultQueryShippingRecord")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def generateTokenFromCreditCard(self):
         self.apiRequest = urljoin(self.apiUrl,"GenerateTokenFromCreditCard")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
     
     def getCreditCardFromToken(self):
         self.apiRequest = urljoin(self.apiUrl,"GetCreditCardFromToken")
-        return(Blaze.performRequest(self))
+        return(RestGateway.performRequest(self))
